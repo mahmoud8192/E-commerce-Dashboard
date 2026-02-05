d3493c45092dcf3267e46aea73dcabbbeea2f7bd
Mahmoud Jaber <mahmoudprjaber075@gmail.com>
2026-02-05 15:39:21 +0300

    ignore some important files

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { DashboardProvider } from "./context/DashboardContext.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
