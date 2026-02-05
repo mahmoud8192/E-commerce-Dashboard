d2ca1e6fc4dd33785476ebfed9299445505e49b7
Mahmoud Jaber <mahmoudprjaber075@gmail.com>
2026-02-05 17:12:49 +0300

    remove un-neccery imports

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import App from "./App.jsx";
 import "./index.css";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { DashboardProvider } from "./context/DashboardContext.jsx";
-import TopProducts from "./components/dashboard/TopProducts.jsx";
-import StatCard from "./components/dashboard/StatCard.jsx";
-import { DollarSignIcon } from "lucide-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
